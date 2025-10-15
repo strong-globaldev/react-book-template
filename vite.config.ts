406f96442d331bf7c4cf8ae8ab699f4a04812db1
Trần Văn Khôi <strong.tran@vietis.com.vn>
2025-10-15 16:41:13 +0700

    feat(setup): vite originail set up

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'esbuild',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
-  },
-  base: './'
 })
